8fd74966389f286a8e24f8d2f0ecc555dac7347c
tybatt <tbattaglia@csumb.edu>
2014-03-12 18:53:42 -0700

    blah

---
+++ OnlineStore/src/Book.java
@@ -0,0 +1,32 @@
+public class Book extends Product
+{
+    private String author;
+
+    public Book()
+    {
+        super();
+        author = "";
+        addCount();
+    }
+
+    public void setAuthor(String author)
+    {
+        this.author = author;
+    }
+
+    public String getAuthor()
+    {
+        return author;
+    }
+
+    public String toString()
+    {
+        return super.toString() +
+            "Author:      " + author + "\n";
+    }
+    
+    public void displayContent()
+    {
+    	System.out.println("Author:      " + author + "\n");
+    }
+}
